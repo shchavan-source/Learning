f04d8cc424386eec3e4481f2965ca2ca08a66c94
Shrikant Chavan <shribinipro@Shrikants-MacBook-Pro.local>
2023-06-12 01:33:07 +0800

    first commit

---
+++ src/main/java/org/shchavan/learn/newConcepts/NewConceptsApplication.java
@@ -0,0 +1,13 @@
+package org.shchavan.learn.newConcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/shchavan/learn/newConcepts/NewConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shchavan.learn.newConcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
